2a4ee64a44efd00dc0a1e7997e465c875f714ab1
cumar-dev <your_email@example.com>
2026-05-12 21:59:45 +0300

    completing authentication and authorization

--- job_Portal_Backend/Controllers/ProtectedController.cs
+++ job_Portal_Backend/Controllers/ProtectedController.cs
@@ -8,13 +8,35 @@ namespace job_Portal_Backend.Controllers
     [Route("api/[controller]")]
     public class ProtectedController : ControllerBase
     {
+        // ======================
+        // GET
+        // ======================
         [Authorize(Roles = "Employer,JobSeeker")]
         [HttpGet]
         public IActionResult GetProtectedData()
         {
             return Ok(new
             {
-                Message = "Access granted",
+                Message = "GET works",
+
+                User = User.FindFirst(ClaimTypes.Name)?.Value,
+
+                Role = User.FindFirst(ClaimTypes.Role)?.Value
+            });
+        }
+
+        // ======================
+        // POST
+        // ======================
+        [Authorize(Roles = "Employer,JobSeeker")]
+        [HttpPost]
+        public IActionResult CreateData([FromBody] object data)
+        {
+            return Ok(new
+            {
+                Message = "POST works",
+
+                Data = data,
 
                 User = User.FindFirst(ClaimTypes.Name)?.Value,
 
--- job_Portal_Backend/obj/Debug/net8.0/job_Portal_Backend.AssemblyInfo.cs
+++ job_Portal_Backend/obj/Debug/net8.0/job_Portal_Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("job_Portal_Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d4bb9001799c55b68269f74e8bf055bd7063f3c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6581e9bd613531a830078ce3a6dcbfa10e7e872b")]
 [assembly: System.Reflection.AssemblyProductAttribute("job_Portal_Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("job_Portal_Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
