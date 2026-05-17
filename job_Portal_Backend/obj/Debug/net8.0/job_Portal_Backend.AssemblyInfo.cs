121ce116bd89007ebdfbf8cad5a7c9facde54de0
cumar-dev <your_email@example.com>
2026-05-17 20:31:40 +0300

    Nadiifin: Waxaa laga saaray bin iyo obj kaydka Git

--- job_Portal_Backend/obj/Debug/net8.0/job_Portal_Backend.AssemblyInfo.cs
+++ job_Portal_Backend/obj/Debug/net8.0/job_Portal_Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("job_Portal_Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bd826d824a75a07913e6571a55e16cc2d62f93c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33689d6caaeb7da74e1f29dd3cd9dbfdf308c180")]
 [assembly: System.Reflection.AssemblyProductAttribute("job_Portal_Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("job_Portal_Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
